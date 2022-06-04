99a5143cbbd38c2846df6cbe689fc37d79f92e1e
Simon Brand <simon.brand@postadigitale.de>
2022-06-04 02:44:41 +0200

    Move ArchLinuxConfig to distributions

--- src/secure_squash_root/__init__.py
+++ src/secure_squash_root/__init__.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 import os
 import shutil
-from collections.abc import Mapping
 import secure_squash_root.cmdline as cmdline
 import secure_squash_root.efi as efi
 from secure_squash_root.config import TMPDIR, KERNEL_PARAM_BASE, \
@@ -9,8 +8,8 @@ from secure_squash_root.config import TMPDIR, KERNEL_PARAM_BASE, \
 from secure_squash_root.exec import exec_binary
 from secure_squash_root.file_op import read_text_from, write_str_to
 from secure_squash_root.image import mksquashfs, veritysetup_image
-from secure_squash_root.initramfs import merge_initramfs_images
 from secure_squash_root.distributions.base import DistributionConfig
+from secure_squash_root.distributions.arch import ArchLinuxConfig
 
 
 DEFAULT_CONFIG = {
@@ -63,59 +62,6 @@ def build_and_sign_kernel(config: Config, vmlinuz: str, initramfs: str,
         else:
             os.rename(out, "{}.bak".format(out))
     shutil.move(tmp_efi_file, out)
-
-
-class ArchLinuxConfig(DistributionConfig):
-    _microcode_paths: [str] = ["/boot/intel-ucode.img", "/boot/amd-ucode.img"]
-    _preset_map: Mapping[str, str] = {"default": ""}
-
-    def file_name(self, kernel: str, preset: str) -> str:
-        preset_name = self._preset_map.get(preset, preset)
-        kernel_name = self._kernel_to_name(kernel)
-        if preset_name == "":
-            return kernel_name
-        else:
-            return "{}_{}".format(kernel_name, preset_name)
-
-    def efi_dirname(self) -> str:
-        return "Arch"
-
-    def _kernel_to_name(self, kernel: str) -> str:
-        pkgbase_file = os.path.join(self._modules_dir, kernel, "pkgbase")
-        return read_text_from(pkgbase_file).strip()
-
-    def vmlinuz(self, kernel: str) -> str:
-        return os.path.join(self._modules_dir, kernel, "vmlinuz")
-
-    def build_initramfs_with_microcode(self, kernel: str,
-                                       preset: str) -> str:
-        name = self._kernel_to_name(kernel)
-        config = read_text_from(os.path.join(
-            "/etc/mkinitcpio.d", "{}.preset".format(name)))
-        base_path = os.path.join(TMPDIR, "{}-{}".format(
-            name, preset))
-        initcpio_image = "{}.initcpio".format(base_path)
-        preset_path = "{}.preset".format(base_path)
-        write_config = "{}\nPRESETS=('{p}')\n{p}_image={}\n".format(
-            config,
-            initcpio_image,
-            p=preset)
-        write_str_to(preset_path, write_config)
-        exec_binary(["mkinitcpio", "-p", preset_path])
-
-        merged_initramfs = "{}.image".format(base_path)
-        merge_initramfs_images(initcpio_image, self._microcode_paths,
-                               merged_initramfs)
-        return merged_initramfs
-
-    def list_kernel(self) -> [str]:
-        return os.listdir(self._modules_dir)
-
-    def list_kernel_presets(self, kernel: str) -> [str]:
-        name = self._kernel_to_name(kernel)
-        run = "/usr/lib/secure-squash-root/mkinitcpio_list_presets"
-        presets_str = exec_binary([run, name])[0].decode()
-        return presets_str.strip().split("\n")
 
 
 class RunProgAndCleanup:
