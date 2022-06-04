97cfd58481172c545891d51732b44ddea4faefc8
Simon Brand <simon.brand@postadigitale.de>
2022-06-04 02:35:21 +0200

    Tests: add ConfigTest

--- tests/unit/tests.py
+++ tests/unit/tests.py
@@ -1,5 +1,6 @@
 import unittest
 from tests.unit.cmdline import CmdlineTest
+from tests.unit.config import ConfigTest
 from tests.unit.efi import EfiTest
 from tests.unit.exec import ExecTest
 from tests.unit.file_op import FileOPTest
@@ -12,6 +13,7 @@ from tests.unit.pep_checker import Pep8Test
 def test_suite():
     suite = unittest.TestSuite([
         unittest.makeSuite(CmdlineTest),
+        unittest.makeSuite(ConfigTest),
         unittest.makeSuite(EfiTest),
         unittest.makeSuite(ExecTest),
         unittest.makeSuite(FileOPTest),
