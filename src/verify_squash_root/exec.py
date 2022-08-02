e11327d235731ae8a6e962f136b98d98a5f8150f
Simon Brand <simon.brand@postadigitale.de>
2022-08-03 00:19:29 +0200

    exec: exec_binary: add exception to format error on print

--- src/verify_squash_root/exec.py
+++ src/verify_squash_root/exec.py
@@ -2,11 +2,29 @@ import subprocess
 from typing import List, Tuple
 
 
+class ExecBinaryError(ChildProcessError):
+
+    def __init__(self, cmd: List[str], stdout: bytes, stderr: bytes):
+        self.__cmd = cmd
+        self.__stderr = stderr
+        super().__init__([cmd, (stdout, stderr)])
+
+    def __str__(self):
+        return "Failed to execute '{}', error: {}".format(
+            " ".join(self.__cmd), str(self.__stderr)[1:])
+
+
 def exec_binary(cmd: List[str], expect_returncode: int = 0) \
         -> Tuple[bytes, bytes]:
-    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE,
-                            stderr=subprocess.PIPE)
+    if len(cmd) == 0:
+        raise ChildProcessError("Cannot execute empty cmd")
+    try:
+        proc = subprocess.Popen(cmd, stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE)
+    except FileNotFoundError:
+        raise ChildProcessError(
+            "Binary not found: {}, is it installed?".format(cmd[0]))
     result = proc.communicate()
     if proc.returncode != expect_returncode:
-        raise ChildProcessError([cmd, result])
+        raise ExecBinaryError(cmd, result[0], result[1])
     return result
--- tests/unit/exec.py
+++ tests/unit/exec.py
@@ -1,22 +1,26 @@
 import unittest
-from verify_squash_root.exec import exec_binary
+from verify_squash_root.exec import exec_binary, ExecBinaryError
 
 
 class ExecTest(unittest.TestCase):
 
     def test__exec_binary__exit_code(self):
-        exit_3_cmd = ["dash", "-c", "exit 3"]
+        exit_3_cmd = ["dash", "-c", "echo err str >&2 ; exit 3"]
         self.assertEqual(
                 exec_binary(exit_3_cmd, 3),
-                (b'', b''))
-        with self.assertRaises(ChildProcessError) as e_ctx:
+                (b'', b'err str\n'))
+        with self.assertRaises(ExecBinaryError) as e_ctx:
             exec_binary(exit_3_cmd)
-        exception = [exit_3_cmd, (b'', b'')],
+        exception = [exit_3_cmd, (b'', b'err str\n')],
         self.assertEqual(e_ctx.exception.args, exception)
 
-        with self.assertRaises(ChildProcessError) as e_ctx:
+        with self.assertRaises(ExecBinaryError) as e_ctx:
             exec_binary(exit_3_cmd, 1)
         self.assertEqual(e_ctx.exception.args, exception)
+        self.assertEqual(
+            str(e_ctx.exception),
+            "Failed to execute 'dash -c echo err str >&2 "
+            "; exit 3', error: 'err str\\n'")
 
     def test__exec_binary__result(self):
         result = exec_binary(["dash", "-c",
@@ -24,3 +28,16 @@ class ExecTest(unittest.TestCase):
                               'printf StdErrStr\\\\n456 >&2'])
         self.assertEqual(result,
                          (b'StdOutStr\n123', b'StdErrStr\n456'))
+
+    def test__exec_binary__empty(self):
+        with self.assertRaises(ChildProcessError) as e_ctx:
+            exec_binary([])
+        self.assertEqual(e_ctx.exception.args,
+                         ("Cannot execute empty cmd",))
+
+    def test__exec_binary__not_found(self):
+        with self.assertRaises(ChildProcessError) as e_ctx:
+            exec_binary(["notexistingbin", "-c", "param"])
+        self.assertEqual(
+            e_ctx.exception.args,
+            ("Binary not found: notexistingbin, is it installed?",))
