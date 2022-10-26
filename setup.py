e2bac13a9ca7198eb91ecdfa5f4b9084b4b7746f
Simon Brand <simon.brand@postadigitale.de>
2022-10-27 01:41:23 +0200

    ci: add test for python 3.11, remove python 3.8 from setup.py

--- setup.py
+++ setup.py
@@ -16,9 +16,9 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         'Operating System :: Linux :: Arch Linux',
         'Environment :: Console'
     ],
