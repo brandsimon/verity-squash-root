a81dc0321e0f4cc9303f911ca7fe077b92f0bc04
Simon Brand <simon.brand@postadigitale.de>
2023-03-24 12:45:45 +0100

    parsing: remove unneeded code

--- src/verity_squash_root/parsing.py
+++ src/verity_squash_root/parsing.py
@@ -4,8 +4,7 @@ from collections.abc import Mapping
 def info_to_dict(output: str, sep: str = ":") -> Mapping[str, str]:
     result = {}
     for line in output.split("\n"):
-        line = line.strip()
-        if line == "" or sep not in line:
+        if sep not in line:
             continue
         s = line.split(sep, 2)
         key = s[0].strip()
